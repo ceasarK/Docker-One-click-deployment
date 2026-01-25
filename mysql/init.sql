4c52b79f5e8d2a535adc506b03612840105c6b78
Ash <1004451756@qq.com>
2026-01-25 10:31:43 +0800

    fix: 多余配置删除

--- mysql/init.sql
+++ mysql/init.sql
@@ -4,7 +4,7 @@ SOURCE /docker-entrypoint-initdb.d/sqls/nacos-mysql-schema.sql;
 --初始化自己的nacos配置
 use nacos;
 SOURCE /docker-entrypoint-initdb.d/sqls/nacos-zjmfz-data.sql;
---初始化业务数据
+--初始化业务数据 数据库名称用下划线“_”分割，中划线有问题“-”，不会创建对应库
 CREATE DATABASE IF NOT EXISTS zjmfz_mini_rig CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 use zjmfz_mini_rig;
 SOURCE /docker-entrypoint-initdb.d/sqls/zjmfz_mini_rig.sql;
